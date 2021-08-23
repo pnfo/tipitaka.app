ba054159dbe8050ad61716869b89c0a4d3a63f62
pathnirvana <pathnirvana@gmail.com>
2021-08-23 15:08:24 +0700

    fix issue 19 pointed by pndaza

--- scripts/pali-script.mjs
+++ scripts/pali-script.mjs
@@ -200,7 +200,7 @@ function beautify_mymr(text, script, rendType = '') { // new unicode 5.1 spec ht
     // following code for tall aa is from https://www.facebook.com/pndaza.mlm
     text = text.replace(/([ခဂငဒပဝ]ေ?)\u102c/g, "$1\u102b"); // aa to tall aa
     text = text.replace(/(က္ခ|န္ဒ|ပ္ပ|မ္ပ)(ေ?)\u102b/g, "$1$2\u102c"); // restore back tall aa to aa for some pattern
-    return text.replace(/(ဒ္ဓ|ဒွ)(ေ?)\u102c/g, "$1\u102b");
+    return text.replace(/(ဒ္ဓ|ဒွ)(ေ?)\u102c/g, "$1$2\u102b");
 }
 function un_beautify_mymr(text) {  // reverse of beautify above
     text = text.replace(/\u102B/g, 'ာ');
